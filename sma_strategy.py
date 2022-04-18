26179a18d24719933c94a8b321bcf61f0375ee37
luckqk <1061745892@qq.com>
2022-04-18 22:02:48 +0800

    sma strategy

--- sma_strategy.py
+++ sma_strategy.py
@@ -39,4 +39,37 @@ data[['returns', 'strategy']].sum()
 # e^([ln(pday1/pday2)+ln(pday2/pday3)])
 data[['returns', 'strategy']].cumsum().apply(np.exp).plot(figsize=(10, 6))
 
+# 风险评估
+# sharp ratio 夏普率
+# 年化收益率
+data[['returns', 'strategy']].mean() * 252
+# 年化风险
+data[['returns', 'strategy']].std() * 252 ** 0.5
+
+# drawdown 回撤
+# cumret  accumulative return
+data['cumret'] = data['strategy'].cumsum().apply(np.exp)
+# cummax 累计收益最大值(区间最大值)
+data['cummax'] = data['cumret'].cummax()
+drawdown = (data['cummax'] - data['cumret'])
+# 最大回撤
+drawdown.max()
+
+
+# 优化：设置阈值，拒绝频繁信号
+hs300 = pro.daily(ts_code='hs300', start_date='20160630', end_date='20170630')
+hs300.rename(columns={'close': 'price'}, inplace=True)
+hs300.set_index('trade_date',inplace = True)
+hs300['SMA_10'] = hs300['price'].rolling(10).mean()
+hs300['SMA_60'] = hs300['price'].rolling(60).mean()
+hs300['10-60'] = hs300['SMA_10'] - hs300['SMA_60']
+# 阈值
+SD = 20
+hs300['regime'] = np.where(hs300['10-60'] > SD, 1, 0)
+hs300['regime'] = np.where(hs300['10-60'] < -SD, -1, hs300['regime'])
+hs300['regime'].value_counts()
+
+hs300['Market'] = np.log(hs300['price']/hs300['price'].shift(1))
+hs300['Strategy'] = hs300['regime'].shift(1) * hs300['Market']
+hs300[['Market','Strategy']].cumsum().apply(np.exp).plot(grid=True, figsize = (10,8))
 
